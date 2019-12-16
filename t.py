e80bc2c7ec67de6d24261b28b484da7e32481e65
HariC137 <43471807+HariC137@users.noreply.github.com>
2019-12-16 22:19:57 +0530

    Recquired Changes V-1

--- t.py
+++ t.py
@@ -8,7 +8,6 @@
 
 
 from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtWidgets import QMessageBox
 
 
 class Ui_MainWindow(object):
@@ -21,13 +20,16 @@ class Ui_MainWindow(object):
         self.speed.setGeometry(QtCore.QRect(500, 250, 291, 141))
         self.speed.setObjectName("speed")
         self.batterylvl = QtWidgets.QLCDNumber(self.centralwidget)
-        self.batterylvl.setGeometry(QtCore.QRect(1040, 50, 221, 71))
+        self.batterylvl.setGeometry(QtCore.QRect(980, 50, 221, 71))
         self.batterylvl.setObjectName("batterylvl")
         self.label = QtWidgets.QLabel(self.centralwidget)
-        self.label.setGeometry(QtCore.QRect(600, 210, 91, 16))
+        self.label.setGeometry(QtCore.QRect(500, 200, 221, 51))
+        font = QtGui.QFont()
+        font.setPointSize(20)
+        self.label.setFont(font)
         self.label.setObjectName("label")
         self.label_2 = QtWidgets.QLabel(self.centralwidget)
-        self.label_2.setGeometry(QtCore.QRect(1040, 30, 111, 16))
+        self.label_2.setGeometry(QtCore.QRect(980, 30, 211, 16))
         self.label_2.setObjectName("label_2")
         self.lighton = QtWidgets.QPushButton(self.centralwidget)
         self.lighton.setGeometry(QtCore.QRect(310, 410, 141, 61))
@@ -45,14 +47,34 @@ class Ui_MainWindow(object):
         self.stop.setGeometry(QtCore.QRect(160, 700, 93, 28))
         self.stop.setObjectName("stop")
         self.label_3 = QtWidgets.QLabel(self.centralwidget)
-        self.label_3.setGeometry(QtCore.QRect(20, 40, 201, 16))
+        self.label_3.setGeometry(QtCore.QRect(10, 30, 441, 16))
         self.label_3.setObjectName("label_3")
         self.totalrun = QtWidgets.QLCDNumber(self.centralwidget)
-        self.totalrun.setGeometry(QtCore.QRect(10, 70, 221, 71))
+        self.totalrun.setGeometry(QtCore.QRect(10, 50, 221, 71))
         self.totalrun.setObjectName("totalrun")
         self.Horn = QtWidgets.QPushButton(self.centralwidget)
         self.Horn.setGeometry(QtCore.QRect(580, 480, 141, 61))
         self.Horn.setObjectName("Horn")
+        self.label_4 = QtWidgets.QLabel(self.centralwidget)
+        self.label_4.setGeometry(QtCore.QRect(240, 50, 91, 71))
+        font = QtGui.QFont()
+        font.setPointSize(36)
+        font.setBold(False)
+        font.setWeight(50)
+        self.label_4.setFont(font)
+        self.label_4.setObjectName("label_4")
+        self.label_5 = QtWidgets.QLabel(self.centralwidget)
+        self.label_5.setGeometry(QtCore.QRect(800, 250, 161, 141))
+        font = QtGui.QFont()
+        font.setPointSize(36)
+        self.label_5.setFont(font)
+        self.label_5.setObjectName("label_5")
+        self.label_6 = QtWidgets.QLabel(self.centralwidget)
+        self.label_6.setGeometry(QtCore.QRect(1210, 50, 61, 71))
+        font = QtGui.QFont()
+        font.setPointSize(36)
+        self.label_6.setFont(font)
+        self.label_6.setObjectName("label_6")
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 1280, 26))
@@ -65,27 +87,26 @@ class Ui_MainWindow(object):
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
-
         self.start.clicked.connect(self.clicked)
-
-
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self.label.setText(_translate("MainWindow", "SPEED (KM/H):"))
-        self.label_2.setText(_translate("MainWindow", "BATTERY LVL (%):"))
+        self.label.setText(_translate("MainWindow", "SPEED :"))
+        self.label_2.setText(_translate("MainWindow", "BATTERY LVL :"))
         self.lighton.setText(_translate("MainWindow", "Light ON"))
         self.lightoff.setText(_translate("MainWindow", "Light Off"))
         self.killswitch.setText(_translate("MainWindow", "Kill Switch"))
         self.start.setText(_translate("MainWindow", "START"))
         self.stop.setText(_translate("MainWindow", "STOP"))
-        self.label_3.setText(_translate("MainWindow", "TOTAL DISTANCE COVERED (KM):"))
+        self.label_3.setText(_translate("MainWindow", "TOTAL DISTANCE COVERED :"))
         self.Horn.setText(_translate("MainWindow", "Horn"))
+        self.label_4.setText(_translate("MainWindow", "KM"))
+        self.label_5.setText(_translate("MainWindow", "KM/H"))
+        self.label_6.setText(_translate("MainWindow", "%"))
 
 
-
-    def clicked(self):
+      def clicked(self):
         if self.killswitch.isChecked():
             print('Checked')
             msg_1 = QMessageBox()
@@ -104,10 +125,10 @@ class Ui_MainWindow(object):
 
             x = msg_2.exec_()
 
+
 if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
-    widget = QtWidgets.QWidget()
     MainWindow = QtWidgets.QMainWindow()
     ui = Ui_MainWindow()
     ui.setupUi(MainWindow)
